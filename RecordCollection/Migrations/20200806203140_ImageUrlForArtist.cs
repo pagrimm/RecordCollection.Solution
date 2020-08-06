caaa71b60885333bfb47e64a660fac1ea851bf7a
Spencer Moody <spencer.moody@outlook.com>
2020-08-06 13:53:33 -0700

    Update Artist model to include ImageUrl/views
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>
    Co-authored-by: Sudolphus <micheal.hansen@gmail.com>

---
+++ RecordCollection/Migrations/20200806203140_ImageUrlForArtist.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecordCollection.Migrations
+{
+    public partial class ImageUrlForArtist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Artists",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Artists");
+        }
+    }
+}
